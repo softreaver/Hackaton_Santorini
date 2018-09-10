27ca966fa314643e0154fcce82cf94e8310597bd
Clemens2 <lanau.clemens_bis@gmail.com>
2018-09-10 16:54:31 +0200

    Modification du GameControlerServer, et début de la méthode moveToken du BoardServer

---
+++ BoardServer.js
@@ -0,0 +1,34 @@
+
+function BoardServer() {
+    let activePlayer = [];
+    let listPlayers = [];
+    let tokens = [];
+    let square = [];
+    let building = [];
+
+
+    this.moveToken = function (tokens, square) {
+        // Condition si le token n'est pas null
+        if (tokens !== null) {
+            if (square !== null) {
+                if ("le deplacement n'est pas null") {
+                    console.log(`There is a problem with your deplacement null`);
+                } else {
+                    if ("Le deplacement est au dessus de 2 - condition +1 -1") {
+                        console.log(`You can't move more 1 squares`);
+                    } else {
+                        console.log(`You're new position is .... `);
+                    }
+
+                }
+            }
+            else {
+                console.log(`There is a problem with the square`);
+            }
+        } else {
+            console.log(`There is a problem with the token`);
+        }
+    }
+
+
+}
---
+++ GameControlerServer.js
@@ -0,0 +1,82 @@
+// Import des modules node.js
+
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var fs = require('fs');
+
+// On va écouter sur le port 80
+server.listen(80);
+
+// Route, mappage
+// Ajout d'une route a Express ? 
+app.get("/", (req, resp) => {
+    let ip = req.connection.remoteAddress;
+    console.log(`[${ip}] asked for index.html.`);
+    fs.readFile(`views/test.html`, `utf-8`, function (error, content) {
+        if (error !== null) {
+            console.log(error.message);
+            resp.writeHead(500, { "Content-Type": "text/plain" });
+            resp.end(error.message);
+        } else {
+            resp.writeHead(200, { "Content-Type": "text/html" });
+            resp.end(content);
+        }
+    });
+});
+
+// Récupere les fichiers Js
+app.get('/client/:fileName', (req, resp) => {
+    let ip = req.connection.remoteAddress;
+    let fileName = req.params.fileName;
+    console.log(`[${ip}] asked for ${fileName}.`);
+    resp.sendfile(__dirname + '/client/' + fileName);
+});
+
+// Récupère les fichiers Css 
+app.get('/css/:fileName', (req, resp) => {
+    let ip = req.connection.remoteAddress;
+    let fileName = req.params.fileName;
+    console.log(`[${ip}] asked for ${fileName}.`);
+    resp.sendfile(__dirname + '/css/' + fileName);
+});
+
+// Récupère les fichiers dans le dossier images
+app.get('/img/:fileName', (req, resp) => {
+    let ip = req.connection.remoteAddress;
+    let fileName = req.params.fileName;
+    console.log(`[${ip}] asked for ${fileName}.`);
+    resp.sendfile(__dirname + '/img/' + fileName);
+});
+
+// Récupere les fichiers dans le dossier Model
+app.get('/model/:fileName', (req, resp) => {
+    let ip = req.connection.remoteAddress;
+    let fileName = req.params.fileName;
+    console.log(`[${ip}] asked for ${fileName}.`);
+    resp.sendfile(__dirname + '/model/' + fileName);
+});
+
+// -------------------------------------------------------------------------
+
+
+// WebSocket
+
+// Evenement pour la méthode de Connection
+io.on('connection', function (socket) {
+    console.log(`An user just connect `);
+    socket.emit(`hello`, 'You have been connected !');
+
+    // Evenement pour la méthode moveToken
+    socket.on('moveToken', function (TokenJson, squareJson) {
+        console.log(`Ask for deplacement`);
+        // En attente du BoardServer
+    });
+    // Evenement pour la méthode build
+    socket.on('build', function (TokenJson, squareJson) {
+        console.log(`Ask for building`);
+        // En attente du BoardServer
+    });
+});
+
+console.log('Server online ...');
