882201c312b574f43c3fae4551fc135da4712333
Unknown <b.saiyane@gmail.com>
2018-09-10 16:55:33 +0200

    board

--- client/Board.js
+++ client/Board.js
@@ -1,2 +1,66 @@
 "use strict";
 
+"use strict";
+
+function Board(service) {
+    let playersList = [];
+    let tokenList = [];
+    let squareList = [];
+    let buildingList = [];
+
+
+    // let att1 = service;
+
+    // this.att2;// publics
+
+    this.sendMovePublics = function (token, square) {
+        this.tokenJsonDeToken;
+        this.jsonDeSquare;
+    };
+
+    this.getMovePublics = function (token, square) {
+        this.tokenJson;
+        this.squareJson;
+    };
+
+    this.sendBuildPublics = function (token, square) { };
+
+    this.getBuildPublics = function (square) {
+        this.squareJson;
+    }
+
+
+    this.getPlayersList = function () {
+        return playersList;
+    }
+
+    this.getTokenList = function () {
+        return tokenList;
+    }
+
+    this.getSquareList = function () {
+        return squareList;
+    }
+
+    this.getBuildingList = function () {
+        return buildingList;
+    }
+
+    this.setPlayerList = function () { }
+    this.setTokenList = function () { }
+    this.setSquareList = function () { }
+    this.setBuildingList = function () { }
+
+
+
+    this.addPlayer = function (player) {
+        playersList.push(player);
+    }
+
+
+
+
+    this.removePlayer = function (player) {
+
+    }
+}
