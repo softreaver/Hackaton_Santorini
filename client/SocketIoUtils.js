844d141c7d9a5e2eb2ec653798f7e879082ec52d
Unknown <b.saiyane@gmail.com>
2018-09-10 16:54:29 +0200

    serveur_client
     branch kaiti_client

---
+++ client/SocketIoUtils.js
@@ -0,0 +1,29 @@
+"use strict";
+
+function SocketIoUtils() {
+    let socket = io.connect();
+
+    socket.on('connect_error', error => {
+        console.log(error);
+    });
+
+    socket.on('moveToken', (tokenJson, squareJson) => {
+    });
+
+    this.sendMoveTokenPublics = function (token) {
+        this.Token;
+    };
+
+    this.sendBuildPublics = function (square) {
+        this.square;
+    }
+
+    socket.on('Build', (squareJson) => {
+    });
+
+    this.playerConnectPublics = function (playerJson) { }
+    this.playerLeavePublics = function (playerJson) { }
+    this.turnOfPublics = function (playerJson) { }
+    this.victoryPublics = function (playerJson) { }
+
+}
