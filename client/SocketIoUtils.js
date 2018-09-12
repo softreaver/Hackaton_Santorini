aad642145d6b815fa81aa9fb29bad2a95b600493
kaiti <b.saiyane@gmail.com>
2018-09-12 09:04:10 +0200

    kaiti_client
    modification par Chris

--- client/SocketIoUtils.js
+++ client/SocketIoUtils.js
@@ -32,4 +32,51 @@ var SocketIoUtils = SocketIoUtils || {};
     this.victoryPublics = function (playerJson) { }
 
 
+<<<<<<< Updated upstream
 }(SocketIoUtils));
+=======
+    let socket = io('http://localhost');
+    socket.on('news', function (data) {
+        console.log(data);
+        socket.emit('my other event', { my: 'data' });
+    });
+
+
+    let socket = io.connect('http//localhost');
+    socket.on('news', function (data) {
+        console.log(data);
+        socket.emit('my other event', { my: 'data' });
+    });
+
+
+
+
+    //  Un client se connecte
+    let http = require('http');
+    let fs = require('fs');
+
+    // Chargement du fichier index.html affiché au client
+    let server = http.createServer(function (req, res) {
+        fs.readFile('./views/game.html', 'utf-8', function (error, content) {
+            res.writeHead(200, { "content-type": "text/html" });
+            res.end(content);
+        });
+    });
+
+    // //Chargement de socket.io
+    // let io = require('socket.io').listen(server);
+
+    // // Quant un client se connecte, on le note dans la console
+    // io.sockets.on('connection', function (socket) {
+    //     console.log('Un client est connecté !');
+    // });
+
+    // SocketIoUtils.listen(8080);
+
+    // let socket = io.connect('http://localhost:8080');
+
+    // socket.emit('message', { content: 'Vous êtes bien connecté !', importance: '1' });
+
+    // Le client veut envoyer un 
+}
+>>>>>>> Stashed changes
